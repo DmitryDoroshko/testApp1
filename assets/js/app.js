c32c2938d2ad8ad3251f0f8b10909cf40884f425
Dmitry Doroshko <dmitry.doroshko.99@gmail.com>
2020-01-27 11:43:37 +0200

    Layout done, add city preview and add city form done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,83 @@
-console.log('Initial check!');
+// importing countries and cities data
+import { countriesData } from './countries-data.js';
+import { citiesData } from './cities-data.js';
+
+// Main sections
+let countriesDiv = bindCountries();
+let citiesDiv = bindCities();
+
+// Add city preview elements
+let addCityBtn = bindAddCityButton();
+let addCityPreviewWrapper = bindCityPreviewWrapper();
+
+// Add city form elements
+let addCityFormWrapper = bindCityFormWrapper();
+let cancelBtnForm = bindCancelBtnForm();
+
+// By default showing the first country in the list
+renderCities(citiesDiv, 1, citiesData);
+
+createAddCityBtnListener(addCityBtn, addCityPreviewWrapper, addCityFormWrapper);
+createCancelBtnFormListener(cancelBtnForm, addCityPreviewWrapper, addCityFormWrapper);
+
+function bindCities(countryId) {
+    return document.querySelector('.js-cities');
+}
+
+function bindCountries() {
+    return document.querySelector('.js-countries');
+}
+
+function bindAddCityButton() {
+    return document.querySelector('.js-cities__item-add-btn');
+}
+
+function bindCityPreviewWrapper() {
+    return document.querySelector('.js-cities__item-add-btn-wrapper');
+}
+
+function bindCityFormWrapper() {
+    return document.querySelector('.js-cities__item-add-city-info');
+}
+
+function bindCancelBtnForm() {
+    return document.querySelector('.js-cities__item-add-city-cancel-btn');
+}
+
+function toggleVisuallyHidden(item) {
+    item.classList.add('visually-hidden');
+}
+
+function toggleVisuallyObservable(item) {
+    item.classList.remove('visually-hidden');
+}
+
+function createAddCityBtnListener(addCityBtn, addCityPreviewWrapper, addCityFormWrapper) {
+    addCityBtn.addEventListener('click', () => {
+        // Add city preview gets hidden
+        toggleVisuallyHidden(addCityPreviewWrapper);
+        // Add city form appears
+        toggleVisuallyObservable(addCityFormWrapper);
+    });
+}
+
+function createCancelBtnFormListener(cancelBtnForm, addCityPreviewWrapper, addCityFormWrapper) {
+    cancelBtnForm.addEventListener('click', () => {
+       // Add city form gets hidden
+       toggleVisuallyHidden(addCityFormWrapper);
+       // Add city preview appears
+        toggleVisuallyObservable(addCityPreviewWrapper);
+    });
+}
+
+function createCityItemElement() {
+
+}
+
+function renderCities(citiesDiv, countryId, citiesData) {
+
+}
+
+function clearCitiesFromDOM() {
+
+}
---
+++ assets/js/countries-data.js
@@ -0,0 +1,20 @@
+export const countriesData = [{
+    "id": 1,
+    "title": "United Kingdom",
+    "text": "The United Kingdom of Great Britain and Northern Ireland, commonly known as the United Kingdom(UK) or Britain, is a sovereign state in Europe."
+},
+    {
+        "id": 2,
+        "title": "France",
+        "text": "France, officially the French Republic, is a unitary sovereign state comprising territory in western Europe and several overseas regions and territories."
+    },
+    {
+        "id": 3,
+        "title": "Spain",
+        "text": "Spain, officially the Kingdom of Spain, is a sovereign state located on the Iberian Peninsula in southwestern Europe."
+    },
+    {
+        "id": 4,
+        "title": "Germany",
+        "text": "Germany, officially the Federal Republic of Germany, is a federal parliamentary republic in western - central Europe."
+    }];
